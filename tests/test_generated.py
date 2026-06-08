708d02bb70b386fcd9a19aa61154ea6dfbf1aa15
ryanbotgar <ryan.gartlan@sou.inteli.edu.br>
2026-06-08 01:13:36 -0300

    exp(more-tests-200): cache=true extra=200 fail=false slow=false/0s parallel=True

--- tests/test_generated.py
+++ tests/test_generated.py
@@ -1,7 +1,7 @@
 """Arquivo de testes GERADO automaticamente pelo experimento.
 
-Variação: more-tests-50
-extra_tests=50 include_slow=False slow_seconds=0.0 include_failing=False
+Variação: more-tests-200
+extra_tests=200 include_slow=False slow_seconds=0.0 include_failing=False
 """
 
 from calc import add, mul
@@ -206,3 +206,603 @@ def test_generated_48():
 def test_generated_49():
     assert add(49, 49) == 98
     assert mul(49, 1) == 49
+
+def test_generated_50():
+    assert add(50, 50) == 100
+    assert mul(50, 1) == 50
+
+def test_generated_51():
+    assert add(51, 51) == 102
+    assert mul(51, 1) == 51
+
+def test_generated_52():
+    assert add(52, 52) == 104
+    assert mul(52, 1) == 52
+
+def test_generated_53():
+    assert add(53, 53) == 106
+    assert mul(53, 1) == 53
+
+def test_generated_54():
+    assert add(54, 54) == 108
+    assert mul(54, 1) == 54
+
+def test_generated_55():
+    assert add(55, 55) == 110
+    assert mul(55, 1) == 55
+
+def test_generated_56():
+    assert add(56, 56) == 112
+    assert mul(56, 1) == 56
+
+def test_generated_57():
+    assert add(57, 57) == 114
+    assert mul(57, 1) == 57
+
+def test_generated_58():
+    assert add(58, 58) == 116
+    assert mul(58, 1) == 58
+
+def test_generated_59():
+    assert add(59, 59) == 118
+    assert mul(59, 1) == 59
+
+def test_generated_60():
+    assert add(60, 60) == 120
+    assert mul(60, 1) == 60
+
+def test_generated_61():
+    assert add(61, 61) == 122
+    assert mul(61, 1) == 61
+
+def test_generated_62():
+    assert add(62, 62) == 124
+    assert mul(62, 1) == 62
+
+def test_generated_63():
+    assert add(63, 63) == 126
+    assert mul(63, 1) == 63
+
+def test_generated_64():
+    assert add(64, 64) == 128
+    assert mul(64, 1) == 64
+
+def test_generated_65():
+    assert add(65, 65) == 130
+    assert mul(65, 1) == 65
+
+def test_generated_66():
+    assert add(66, 66) == 132
+    assert mul(66, 1) == 66
+
+def test_generated_67():
+    assert add(67, 67) == 134
+    assert mul(67, 1) == 67
+
+def test_generated_68():
+    assert add(68, 68) == 136
+    assert mul(68, 1) == 68
+
+def test_generated_69():
+    assert add(69, 69) == 138
+    assert mul(69, 1) == 69
+
+def test_generated_70():
+    assert add(70, 70) == 140
+    assert mul(70, 1) == 70
+
+def test_generated_71():
+    assert add(71, 71) == 142
+    assert mul(71, 1) == 71
+
+def test_generated_72():
+    assert add(72, 72) == 144
+    assert mul(72, 1) == 72
+
+def test_generated_73():
+    assert add(73, 73) == 146
+    assert mul(73, 1) == 73
+
+def test_generated_74():
+    assert add(74, 74) == 148
+    assert mul(74, 1) == 74
+
+def test_generated_75():
+    assert add(75, 75) == 150
+    assert mul(75, 1) == 75
+
+def test_generated_76():
+    assert add(76, 76) == 152
+    assert mul(76, 1) == 76
+
+def test_generated_77():
+    assert add(77, 77) == 154
+    assert mul(77, 1) == 77
+
+def test_generated_78():
+    assert add(78, 78) == 156
+    assert mul(78, 1) == 78
+
+def test_generated_79():
+    assert add(79, 79) == 158
+    assert mul(79, 1) == 79
+
+def test_generated_80():
+    assert add(80, 80) == 160
+    assert mul(80, 1) == 80
+
+def test_generated_81():
+    assert add(81, 81) == 162
+    assert mul(81, 1) == 81
+
+def test_generated_82():
+    assert add(82, 82) == 164
+    assert mul(82, 1) == 82
+
+def test_generated_83():
+    assert add(83, 83) == 166
+    assert mul(83, 1) == 83
+
+def test_generated_84():
+    assert add(84, 84) == 168
+    assert mul(84, 1) == 84
+
+def test_generated_85():
+    assert add(85, 85) == 170
+    assert mul(85, 1) == 85
+
+def test_generated_86():
+    assert add(86, 86) == 172
+    assert mul(86, 1) == 86
+
+def test_generated_87():
+    assert add(87, 87) == 174
+    assert mul(87, 1) == 87
+
+def test_generated_88():
+    assert add(88, 88) == 176
+    assert mul(88, 1) == 88
+
+def test_generated_89():
+    assert add(89, 89) == 178
+    assert mul(89, 1) == 89
+
+def test_generated_90():
+    assert add(90, 90) == 180
+    assert mul(90, 1) == 90
+
+def test_generated_91():
+    assert add(91, 91) == 182
+    assert mul(91, 1) == 91
+
+def test_generated_92():
+    assert add(92, 92) == 184
+    assert mul(92, 1) == 92
+
+def test_generated_93():
+    assert add(93, 93) == 186
+    assert mul(93, 1) == 93
+
+def test_generated_94():
+    assert add(94, 94) == 188
+    assert mul(94, 1) == 94
+
+def test_generated_95():
+    assert add(95, 95) == 190
+    assert mul(95, 1) == 95
+
+def test_generated_96():
+    assert add(96, 96) == 192
+    assert mul(96, 1) == 96
+
+def test_generated_97():
+    assert add(97, 97) == 194
+    assert mul(97, 1) == 97
+
+def test_generated_98():
+    assert add(98, 98) == 196
+    assert mul(98, 1) == 98
+
+def test_generated_99():
+    assert add(99, 99) == 198
+    assert mul(99, 1) == 99
+
+def test_generated_100():
+    assert add(100, 100) == 200
+    assert mul(100, 1) == 100
+
+def test_generated_101():
+    assert add(101, 101) == 202
+    assert mul(101, 1) == 101
+
+def test_generated_102():
+    assert add(102, 102) == 204
+    assert mul(102, 1) == 102
+
+def test_generated_103():
+    assert add(103, 103) == 206
+    assert mul(103, 1) == 103
+
+def test_generated_104():
+    assert add(104, 104) == 208
+    assert mul(104, 1) == 104
+
+def test_generated_105():
+    assert add(105, 105) == 210
+    assert mul(105, 1) == 105
+
+def test_generated_106():
+    assert add(106, 106) == 212
+    assert mul(106, 1) == 106
+
+def test_generated_107():
+    assert add(107, 107) == 214
+    assert mul(107, 1) == 107
+
+def test_generated_108():
+    assert add(108, 108) == 216
+    assert mul(108, 1) == 108
+
+def test_generated_109():
+    assert add(109, 109) == 218
+    assert mul(109, 1) == 109
+
+def test_generated_110():
+    assert add(110, 110) == 220
+    assert mul(110, 1) == 110
+
+def test_generated_111():
+    assert add(111, 111) == 222
+    assert mul(111, 1) == 111
+
+def test_generated_112():
+    assert add(112, 112) == 224
+    assert mul(112, 1) == 112
+
+def test_generated_113():
+    assert add(113, 113) == 226
+    assert mul(113, 1) == 113
+
+def test_generated_114():
+    assert add(114, 114) == 228
+    assert mul(114, 1) == 114
+
+def test_generated_115():
+    assert add(115, 115) == 230
+    assert mul(115, 1) == 115
+
+def test_generated_116():
+    assert add(116, 116) == 232
+    assert mul(116, 1) == 116
+
+def test_generated_117():
+    assert add(117, 117) == 234
+    assert mul(117, 1) == 117
+
+def test_generated_118():
+    assert add(118, 118) == 236
+    assert mul(118, 1) == 118
+
+def test_generated_119():
+    assert add(119, 119) == 238
+    assert mul(119, 1) == 119
+
+def test_generated_120():
+    assert add(120, 120) == 240
+    assert mul(120, 1) == 120
+
+def test_generated_121():
+    assert add(121, 121) == 242
+    assert mul(121, 1) == 121
+
+def test_generated_122():
+    assert add(122, 122) == 244
+    assert mul(122, 1) == 122
+
+def test_generated_123():
+    assert add(123, 123) == 246
+    assert mul(123, 1) == 123
+
+def test_generated_124():
+    assert add(124, 124) == 248
+    assert mul(124, 1) == 124
+
+def test_generated_125():
+    assert add(125, 125) == 250
+    assert mul(125, 1) == 125
+
+def test_generated_126():
+    assert add(126, 126) == 252
+    assert mul(126, 1) == 126
+
+def test_generated_127():
+    assert add(127, 127) == 254
+    assert mul(127, 1) == 127
+
+def test_generated_128():
+    assert add(128, 128) == 256
+    assert mul(128, 1) == 128
+
+def test_generated_129():
+    assert add(129, 129) == 258
+    assert mul(129, 1) == 129
+
+def test_generated_130():
+    assert add(130, 130) == 260
+    assert mul(130, 1) == 130
+
+def test_generated_131():
+    assert add(131, 131) == 262
+    assert mul(131, 1) == 131
+
+def test_generated_132():
+    assert add(132, 132) == 264
+    assert mul(132, 1) == 132
+
+def test_generated_133():
+    assert add(133, 133) == 266
+    assert mul(133, 1) == 133
+
+def test_generated_134():
+    assert add(134, 134) == 268
+    assert mul(134, 1) == 134
+
+def test_generated_135():
+    assert add(135, 135) == 270
+    assert mul(135, 1) == 135
+
+def test_generated_136():
+    assert add(136, 136) == 272
+    assert mul(136, 1) == 136
+
+def test_generated_137():
+    assert add(137, 137) == 274
+    assert mul(137, 1) == 137
+
+def test_generated_138():
+    assert add(138, 138) == 276
+    assert mul(138, 1) == 138
+
+def test_generated_139():
+    assert add(139, 139) == 278
+    assert mul(139, 1) == 139
+
+def test_generated_140():
+    assert add(140, 140) == 280
+    assert mul(140, 1) == 140
+
+def test_generated_141():
+    assert add(141, 141) == 282
+    assert mul(141, 1) == 141
+
+def test_generated_142():
+    assert add(142, 142) == 284
+    assert mul(142, 1) == 142
+
+def test_generated_143():
+    assert add(143, 143) == 286
+    assert mul(143, 1) == 143
+
+def test_generated_144():
+    assert add(144, 144) == 288
+    assert mul(144, 1) == 144
+
+def test_generated_145():
+    assert add(145, 145) == 290
+    assert mul(145, 1) == 145
+
+def test_generated_146():
+    assert add(146, 146) == 292
+    assert mul(146, 1) == 146
+
+def test_generated_147():
+    assert add(147, 147) == 294
+    assert mul(147, 1) == 147
+
+def test_generated_148():
+    assert add(148, 148) == 296
+    assert mul(148, 1) == 148
+
+def test_generated_149():
+    assert add(149, 149) == 298
+    assert mul(149, 1) == 149
+
+def test_generated_150():
+    assert add(150, 150) == 300
+    assert mul(150, 1) == 150
+
+def test_generated_151():
+    assert add(151, 151) == 302
+    assert mul(151, 1) == 151
+
+def test_generated_152():
+    assert add(152, 152) == 304
+    assert mul(152, 1) == 152
+
+def test_generated_153():
+    assert add(153, 153) == 306
+    assert mul(153, 1) == 153
+
+def test_generated_154():
+    assert add(154, 154) == 308
+    assert mul(154, 1) == 154
+
+def test_generated_155():
+    assert add(155, 155) == 310
+    assert mul(155, 1) == 155
+
+def test_generated_156():
+    assert add(156, 156) == 312
+    assert mul(156, 1) == 156
+
+def test_generated_157():
+    assert add(157, 157) == 314
+    assert mul(157, 1) == 157
+
+def test_generated_158():
+    assert add(158, 158) == 316
+    assert mul(158, 1) == 158
+
+def test_generated_159():
+    assert add(159, 159) == 318
+    assert mul(159, 1) == 159
+
+def test_generated_160():
+    assert add(160, 160) == 320
+    assert mul(160, 1) == 160
+
+def test_generated_161():
+    assert add(161, 161) == 322
+    assert mul(161, 1) == 161
+
+def test_generated_162():
+    assert add(162, 162) == 324
+    assert mul(162, 1) == 162
+
+def test_generated_163():
+    assert add(163, 163) == 326
+    assert mul(163, 1) == 163
+
+def test_generated_164():
+    assert add(164, 164) == 328
+    assert mul(164, 1) == 164
+
+def test_generated_165():
+    assert add(165, 165) == 330
+    assert mul(165, 1) == 165
+
+def test_generated_166():
+    assert add(166, 166) == 332
+    assert mul(166, 1) == 166
+
+def test_generated_167():
+    assert add(167, 167) == 334
+    assert mul(167, 1) == 167
+
+def test_generated_168():
+    assert add(168, 168) == 336
+    assert mul(168, 1) == 168
+
+def test_generated_169():
+    assert add(169, 169) == 338
+    assert mul(169, 1) == 169
+
+def test_generated_170():
+    assert add(170, 170) == 340
+    assert mul(170, 1) == 170
+
+def test_generated_171():
+    assert add(171, 171) == 342
+    assert mul(171, 1) == 171
+
+def test_generated_172():
+    assert add(172, 172) == 344
+    assert mul(172, 1) == 172
+
+def test_generated_173():
+    assert add(173, 173) == 346
+    assert mul(173, 1) == 173
+
+def test_generated_174():
+    assert add(174, 174) == 348
+    assert mul(174, 1) == 174
+
+def test_generated_175():
+    assert add(175, 175) == 350
+    assert mul(175, 1) == 175
+
+def test_generated_176():
+    assert add(176, 176) == 352
+    assert mul(176, 1) == 176
+
+def test_generated_177():
+    assert add(177, 177) == 354
+    assert mul(177, 1) == 177
+
+def test_generated_178():
+    assert add(178, 178) == 356
+    assert mul(178, 1) == 178
+
+def test_generated_179():
+    assert add(179, 179) == 358
+    assert mul(179, 1) == 179
+
+def test_generated_180():
+    assert add(180, 180) == 360
+    assert mul(180, 1) == 180
+
+def test_generated_181():
+    assert add(181, 181) == 362
+    assert mul(181, 1) == 181
+
+def test_generated_182():
+    assert add(182, 182) == 364
+    assert mul(182, 1) == 182
+
+def test_generated_183():
+    assert add(183, 183) == 366
+    assert mul(183, 1) == 183
+
+def test_generated_184():
+    assert add(184, 184) == 368
+    assert mul(184, 1) == 184
+
+def test_generated_185():
+    assert add(185, 185) == 370
+    assert mul(185, 1) == 185
+
+def test_generated_186():
+    assert add(186, 186) == 372
+    assert mul(186, 1) == 186
+
+def test_generated_187():
+    assert add(187, 187) == 374
+    assert mul(187, 1) == 187
+
+def test_generated_188():
+    assert add(188, 188) == 376
+    assert mul(188, 1) == 188
+
+def test_generated_189():
+    assert add(189, 189) == 378
+    assert mul(189, 1) == 189
+
+def test_generated_190():
+    assert add(190, 190) == 380
+    assert mul(190, 1) == 190
+
+def test_generated_191():
+    assert add(191, 191) == 382
+    assert mul(191, 1) == 191
+
+def test_generated_192():
+    assert add(192, 192) == 384
+    assert mul(192, 1) == 192
+
+def test_generated_193():
+    assert add(193, 193) == 386
+    assert mul(193, 1) == 193
+
+def test_generated_194():
+    assert add(194, 194) == 388
+    assert mul(194, 1) == 194
+
+def test_generated_195():
+    assert add(195, 195) == 390
+    assert mul(195, 1) == 195
+
+def test_generated_196():
+    assert add(196, 196) == 392
+    assert mul(196, 1) == 196
+
+def test_generated_197():
+    assert add(197, 197) == 394
+    assert mul(197, 1) == 197
+
+def test_generated_198():
+    assert add(198, 198) == 396
+    assert mul(198, 1) == 198
+
+def test_generated_199():
+    assert add(199, 199) == 398
+    assert mul(199, 1) == 199
