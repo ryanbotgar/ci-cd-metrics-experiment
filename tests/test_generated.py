40aa598e9cf7a4508594821d9922cd3fca57abfd
ryanbotgar <ryan.gartlan@sou.inteli.edu.br>
2026-06-08 01:14:34 -0300

    exp(slow-test-15s): cache=true extra=0 fail=false slow=true/15s parallel=True

--- tests/test_generated.py
+++ tests/test_generated.py
@@ -1,7 +1,7 @@
 """Arquivo de testes GERADO automaticamente pelo experimento.
 
-Variação: slow-test-5s
-extra_tests=0 include_slow=True slow_seconds=5.0 include_failing=False
+Variação: slow-test-15s
+extra_tests=0 include_slow=True slow_seconds=15.0 include_failing=False
 """
 
 import time
@@ -10,5 +10,5 @@ from calc import is_prime
 
 
 def test_generated_slow():
-    time.sleep(5.0)
+    time.sleep(15.0)
     assert is_prime(7) is True
