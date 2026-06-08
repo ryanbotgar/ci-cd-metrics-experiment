70b7731541f7354b7cdcb84d8949391f4ed39396
ryanbotgar <ryan.gartlan@sou.inteli.edu.br>
2026-06-08 01:12:38 -0300

    exp(failing-1): cache=true extra=0 fail=true slow=false/0s parallel=True

--- tests/test_generated.py
+++ tests/test_generated.py
@@ -1,11 +1,12 @@
 """Arquivo de testes GERADO automaticamente pelo experimento.
 
-Variação: baseline-2
-extra_tests=0 include_slow=False slow_seconds=0.0 include_failing=False
+Variação: failing-1
+extra_tests=0 include_slow=False slow_seconds=0.0 include_failing=True
 """
 
 from calc import add
 
 
-def test_generated_placeholder():
-    assert add(1, 1) == 2
+def test_generated_failing():
+    # Falha proposital para a variação 'failing'.
+    assert add(2, 2) == 5
